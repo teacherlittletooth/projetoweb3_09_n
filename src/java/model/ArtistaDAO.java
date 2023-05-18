badc0899c3b3e42d76919680de196c7d5d507b47
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-05-17 21:58:07 -0300

    Add files via upload

--- src/java/model/ArtistaDAO.java
+++ src/java/model/ArtistaDAO.java
@@ -2,7 +2,9 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ArtistaDAO {
     private static Connection conn;
@@ -30,4 +32,82 @@ public class ArtistaDAO {
         prep.execute();
         prep.close();
     }
-}
+    
+    
+    //SELECT
+    public ArrayList<Artista> listAll() throws SQLException {
+        //Criar lista vazia
+        ArrayList<Artista> list = new ArrayList<>();
+        
+        //Criar query para pesquisa no BD
+        String query = "SELECT * FROM artistas";
+        
+        //Preparar query para executar no BD
+        PreparedStatement prep = conn.prepareStatement(query);
+        
+        //Pegar o resultado da pesquina no BD
+        ResultSet res = prep.executeQuery();
+        
+        //Varrer "res" mapeando os registros do BD
+        //Para objetos da classe "Artista"
+        while(res.next()) {
+            //Criar objeto Artista vazio
+            Artista art = new Artista();
+            
+            //Inserir os valores nos atributos com
+            //os dados vindos das colunas do BD
+            art.setIdArtista(res.getInt("cod_artista"));
+            art.setArtista(res.getString("nome"));
+            art.setGenero(res.getInt("cod_genero"));
+            art.setNacionalidade(res.getString("nacionalidade"));
+            art.setSolo(res.getInt("solo"));
+            
+            //Inserir o objeto completo na lista
+            list.add(art);
+        }
+        
+        //Retornar lista cheia
+        return list;
+    }
+    
+    
+    //SELECT
+    public Artista listById(int i) throws SQLException {
+        String query = "SELECT * FROM artistas "
+                + "WHERE cod_artista = " + i;
+        
+        PreparedStatement prep = conn.prepareStatement(query);
+        ResultSet res = prep.executeQuery();
+        
+        Artista art = new Artista();
+        
+        if(res.next()) {
+            art.setIdArtista(res.getInt("cod_artista"));
+            art.setArtista(res.getString("nome"));
+            art.setGenero(res.getInt("cod_genero"));
+            art.setNacionalidade(res.getString("nacionalidade"));
+            art.setSolo(res.getInt("solo"));
+        }
+        
+        return art;
+    }
+    
+    
+    //UPDATE
+    public void updateArtista(Artista art) throws SQLException {
+        String query = "UPDATE artistas SET nome = ?,"
+                + "cod_genero = ?, nacionalidade = ?,"
+                + "solo = ? WHERE cod_artista = ?";
+        
+        PreparedStatement prep = conn.prepareStatement(query);
+        prep.setString(1, art.getArtista());
+        prep.setInt(2, art.getGenero());
+        prep.setString(3, art.getNacionalidade());
+        prep.setInt(4, art.getSolo());
+        prep.setInt(5, art.getIdArtista());
+        
+        prep.execute();
+        prep.close();
+    }
+    
+}//Fim da classe
--- web/lista.jsp
+++ web/lista.jsp
@@ -4,6 +4,8 @@
     Author     : QI
 --%>
 
+<%@page import="model.ArtistaDAO" %>
+<%@page import="model.Artista" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -14,6 +16,54 @@
     <body>
         <h1>Lista</h1>
         <hr>
+        
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>COD</th>
+                    <th>NOME</th>
+                    <th>GENERO</th>
+                    <th>NACIONALIDADE</th>
+                    <th>SOLO?</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <%
+                    ArtistaDAO adao = new ArtistaDAO();
+                    for(Artista art : adao.listAll()) {
+                %>
+                <tr>
+                    <td><%= art.getIdArtista() %></td>
+                    <td><%= art.getArtista() %></td>
+                    <td><%= art.getGenero() %></td>
+                    <td><%= art.getNacionalidade() %></td>
+                    <td><%= art.getSolo() %></td>
+                    <td>
+                        <a href="ArtistaUpdate?id=">EDITAR</a>
+                    </td>
+                    <td>
+                        <a onclick="confirmDelete()">EXCLUIR</a>
+                    </td>
+                </tr>
+                <% } %>
+            </tbody>
+        </table>
+
+        
+        <hr>
         <a href="home.jsp" >Pagina inicial</a>
+        
+        <script>
+            function confirmDelete() {
+                if(confirm("Deseja realmente excluir?")) {
+                    window.location.replace("ArtistaDelete?cod=");
+                } else {
+                    alert("Exclusão cancelada!");
+                }
+            }
+        </script>
+        
     </body>
 </html>
